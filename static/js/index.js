182bc624ddce983e6e906ff25ddc22ed44995cf4
shivam <shivam.mittal38@gmail.com>
2019-04-01 11:51:33 +0530

    fixed

--- static/js/index.js
+++ static/js/index.js
@@ -10,6 +10,31 @@ $(document).ready(function () {
 //         'slow');
 // });
 
+
+    $('.ml2').each(function(){
+        $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+    });
+
+    anime.timeline({loop: true})
+        .add({
+            targets: '.ml2 .letter',
+            scale: [4,1],
+            opacity: [0,1],
+            translateZ: 0,
+            easing: "easeOutExpo",
+            duration: 1000,
+            delay: function(el, i) {
+                return 70*i;
+            }
+        }).add({
+        targets: '.ml2',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
+
+
    $("#about_us_page").click(function() {
        $('.content3').removeClass('add-margin-top');
        $('.content2').removeClass('add-margin-top');
